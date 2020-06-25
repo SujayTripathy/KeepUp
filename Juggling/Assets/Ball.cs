9684f9f73cf3f52f8bbea02e3041dfb28f3330c7
SujayTripathy <36439006+champfinal@users.noreply.github.com>
2020-06-26 00:12:44 +0530

    base
    Got the ball in, basic kicking on kick, bounding walls.

---
+++ Juggling/Assets/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnMouseUp() {
+        Debug.Log("Click");
+        Rigidbody body= transform.GetComponentInChildren<Rigidbody>();
+        body.AddForce(Random.Range(-4,4),Random.Range(7.5f,12.5f),0,ForceMode.Impulse);
+        body.AddTorque(Random.Range(-5,5),Random.Range(-5,5),Random.Range(-5,5));
+    }
+
+}
