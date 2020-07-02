a81ae608f321021f1a7259c85540d3249864fe08
SujayTripathy <36439006+champfinal@users.noreply.github.com>
2020-07-02 22:08:09 +0530

    Menu/Start button
    Start button works, game is frozen till start is pressed and ball is unclickable till start

---
+++ Juggling/Assets/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menu;
+    [SerializeField]
+    GameObject ball;
+    void Awake(){
+        Time.timeScale=0;
+    }
+    
+    public void StartGame(){
+        Time.timeScale=1;
+        menu.SetActive(false);
+        ball.GetComponentInChildren<SphereCollider>().enabled=true;
+    }
+
+}
