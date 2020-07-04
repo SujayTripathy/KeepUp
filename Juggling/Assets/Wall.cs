469e1c9347a3a1d55384176019b64c3e11a3c1e7
SujayTripathy <36439006+champfinal@users.noreply.github.com>
2020-07-05 04:04:54 +0530

    couple of test builds, better ui scaling, wall is more bouncy

---
+++ Juggling/Assets/Wall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag=="ball"){
+            other.gameObject.GetComponentInChildren<Rigidbody>().AddForce(new Vector3 (-other.gameObject.GetComponentInChildren<Rigidbody>().velocity.x*3,0,0),ForceMode.Impulse);
+        }
+    }
+}
